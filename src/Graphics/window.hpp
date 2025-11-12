ea106f3f4734c6bf3ecfa60d35370b4298c740d8
MalyshevMS <matvey.malyshev29@gmail.com>
2025-11-12 22:32:53 +0300

    added window class

--- src/Graphics/window.hpp
+++ src/Graphics/window.hpp
@@ -1,32 +1,76 @@
-#pragma voice
 #pragma once
 #include <string>
+#include <thread>
 #include <SFML/Graphics.hpp>
 
-class Window
-{
+#pragma GCC diagnostic ignored "-Wunused-result"
+
+class Window {
 private:
-    int width;
-    int height;
-    sf::RenderWindow win;
-    // САУНДКЛАУД
+    unsigned int width; // ГИПОТАЛАМУС
+    unsigned int height;
+    std::string title;
+    sf::RenderWindow* win;
     bool fullscreen = false;
+    bool running = false;
     double dt = 0.0f;
     double LastTime = 0.0f;
     double CurrentTime = 0.0f;
-    enum class CursorState
-    {
+    enum class CursorState {
         standart,
         pointer,
         text,
         notAllowed
     };
+    std::thread winThread;
 
 public:
-    Window(const std::string title, int width, int height)
-    {
+    Window(const std::string& title, unsigned int width, unsigned int height)
+     : title(title), width(width), height(height) {
+        
+    }
+
+    ~Window() {
+        stop();
+    }
+
+    void proceedEvent(const std::optional<sf::Event> event) {
+        // TODO: обработка событий тут
+    }
+
+    void draw() {
+        // TODO: отрисовка через win->draw(...)
+    }
+
+    void loop() {
         sf::RenderWindow window(sf::VideoMode({width, height}), title);
-    };
-    // sf::Vector2f mousePosition = sf::Mouse::getPosition()
-    // std::size_t cursor_x =
+        win = &window;
+        while (running && window.isOpen()) {
+            if (const std::optional event = window.pollEvent()) {
+                if (event->is<sf::Event::Closed>()) {
+                    stop();
+                }
+                proceedEvent(event);
+            }
+
+            window.clear();
+            draw();
+            window.display();
+        }
+    }
+
+    void run() {
+        running = true;
+        winThread = std::thread(&Window::loop, this);
+    }
+    
+    void stop() {
+        running = false;
+        if (win->isOpen())
+            win->close();
+    }
+    
+    bool isRunning() const {
+        return running;
+    }
 };
