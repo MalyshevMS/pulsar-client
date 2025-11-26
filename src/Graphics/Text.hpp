f7e74517e45c5e9df618904add13e4ede6cdbd2e
rewnant <ss.ost@mail.ru>
2025-11-26 22:54:06 +0300

    beforemerge

--- src/Graphics/Text.hpp
+++ src/Graphics/Text.hpp
@@ -5,27 +5,29 @@
 #include <vector>
 #include <SFML/Graphics.hpp>
 
-class Text{
+class Text
+{
 private:
-std::string text = " ";
-int size;
-float R = 0.0
-;float G = 0.0;
-float B= 0.0;
+    std::string text = " ";
+    int size;
+    float R = 0.0;
+    float G = 0.0;
+    float B = 0.0;
 
-sf::Color color = sf::Color({0, 0 , 0});
-std::string user_font = "arial";
-std::vector < std::string > TextStyle = {"Normal", "Bold", "Underlined"};
+    sf::Color color = sf::Color({0, 0, 0});
+    std::string user_font = "arial";
+    std::vector<std::string> TextStyle = {"Normal", "Bold", "Underlined"};
+
 public:
-    Text(std::string text, int size, sf::Color color, std::string user_font){
+    Text(std::string text, int size, sf::Color color, std::string user_font)
+    {
         sf::Font font;
-        if(!font.openFromFile(user_font+ std::to_string(".ttf"))){
-            std::cout <<"Error: Font loading error" << std::endl;
+        if (!font.openFromFile(user_font + ".ttf"))
+        {
+            std::cout << "Error: Font loading error" << std::endl;
         }
         sf::Text MsgText(font);
         MsgText.setFillColor(color);
-        MsgText.setString(std::to_string(text));
-        
+        MsgText.setString(text);
     }
-
 };
--- src/Graphics/window.hpp
+++ src/Graphics/window.hpp
@@ -5,18 +5,20 @@
 
 #pragma GCC diagnostic ignored "-Wunused-result"
 
-class Window {
+class Window
+{
 private:
     unsigned int width; // ГИПОТАЛАМУС
     unsigned int height;
     std::string title;
-    sf::RenderWindow* win;
+    sf::RenderWindow *win;
     bool fullscreen = false;
     bool running = false;
     double dt = 0.0f;
     double LastTime = 0.0f;
     double CurrentTime = 0.0f;
-    enum class CursorState {
+    enum class CursorState
+    {
         standart,
         pointer,
         text,
@@ -25,29 +27,36 @@ private:
     std::thread winThread;
 
 public:
-    Window(const std::string& title, unsigned int width, unsigned int height)
-     : title(title), width(width), height(height) {
-        
+    Window(const std::string &title, unsigned int width, unsigned int height)
+        : title(title), width(width), height(height)
+    {
     }
 
-    ~Window() {
+    ~Window()
+    {
         stop();
     }
 
-    void proceedEvent(const std::optional<sf::Event> event) {
+    void proceedEvent(const std::optional<sf::Event> event)
+    {
         // TODO: обработка событий тут
     }
 
-    void draw() {
+    void draw()
+    {
         // TODO: отрисовка через win->draw(...)
     }
 
-    void loop() {
+    void loop()
+    {
         sf::RenderWindow window(sf::VideoMode({width, height}), title);
         win = &window;
-        while (running && window.isOpen()) {
-            if (const std::optional event = window.pollEvent()) {
-                if (event->is<sf::Event::Closed>()) {
+        while (running && window.isOpen())
+        {
+            if (const std::optional event = window.pollEvent())
+            {
+                if (event->is<sf::Event::Closed>())
+                {
                     stop();
                 }
                 proceedEvent(event);
@@ -58,19 +67,29 @@ public:
             window.display();
         }
     }
-
-    void run() {
+    int GetSizeY()
+    {
+        return height;
+    }
+    int GetSizeX()
+    {
+        return width;
+    }
+    void run()
+    {
         running = true;
         winThread = std::thread(&Window::loop, this);
     }
-    
-    void stop() {
+
+    void stop()
+    {
         running = false;
         if (win->isOpen())
             win->close();
     }
-    
-    bool isRunning() const {
+
+    bool isRunning() const
+    {
         return running;
     }
 };
