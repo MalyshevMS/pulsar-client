7e9b0c8dca0005b07299133774bfdb745626bd7a
MalyshevMS <matvey.malyshev29@gmail.com>
2025-12-02 22:04:27 +0300

    исправил удаление `Window.hpp`

---
+++ src/Graphics/Window.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "WindowBase.hpp"
+#include "Chatlist.hpp"
+
+class Window : public WindowBase {
+private:
+    Chatlist chatlist = Chatlist({"Alice", "Bob", "Charlie Kirk", "Diana", "Eve", "Frank", "Grace", "Heidi", "Ivan", "Klyde", "Kevin"});
+public:
+    Window(const std::string& title, unsigned int width, unsigned int height)
+     : WindowBase(title, width, height) {}
+
+    virtual void proceedEvent(const sf::Event& event) override {
+    
+    }
+
+    virtual void draw() override {
+        chatlist.draw(*win);
+    }
+};
