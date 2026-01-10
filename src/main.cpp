999fa29742bebc4918a8ae11eaa0de3bce30ed79
MalyshevMS <matvey.malyshev29@gmail.com>
2026-01-10 22:53:48 +0300

    полностью изменен формат сообщений с json на прстой текст, база данных изменена с json на sqlite3. есть еще мелкие недоработки

--- src/main.cpp
+++ src/main.cpp
@@ -2,37 +2,19 @@
 #include "API/PulsarAPI.hpp"
 #include "Network/Client.hpp"
 #include "Network/Encryption.hpp"
+#include "Network/Checker.hpp"
 #include <exception>
 #include <csignal>
 
 #ifdef _WIN32
-#include <windows.h>
+#   include <windows.h>
 #endif
 
 #ifndef PULSAR_CHECKER_VERSION
-#error Pulsar Checker not included!
+#   error Pulsar Checker not included!
 #endif
 
-int main(int argc, const char **argv)
-{
-    std::set_terminate([]()
-                       {
-        try {
-            std::exception_ptr eptr = std::current_exception();
-            if (eptr) {
-                try { std::rethrow_exception(eptr); }
-                catch (const std::exception& e) {
-                    std::cerr << "terminate() called after throwing an exception: " << e.what() << std::endl;
-                } catch (...) {
-                    std::cerr << "terminate() called after throwing unknown exception" << std::endl;
-                }
-            } else {
-                std::cerr << "terminate() called without an active exception" << std::endl;
-            }
-        } catch (...) {}
-        std::raise(SIGABRT);
-        std::abort(); });
-
+int main(int argc, const char **argv) {
 #ifdef _WIN32
     SetConsoleCP(65001); // Russian UTF-8 support
     SetConsoleOutputCP(65001);
@@ -52,27 +34,26 @@ int main(int argc, const char **argv)
     serverIP = PULSAR_IP_PRESET;
 #endif
 
-    // if (argc <= 1) {
-    //     std::cout << "Введите имя пользователя: ";
-    //     std::getline(std::cin, name);
-    // }
-    // else {
-    //     name = "@" + std::string(argv[1]);
-    // }
-    // std::cout << "Введите пароль (нажмите ENTER если его нет): ";
-    // std::getline(std::cin, password);
+    if (argc <= 1) {
+        std::cout << "Введите имя пользователя: ";
+        std::getline(std::cin, name);
+    }
+    else {
+        name = "@" + std::string(argv[1]);
+    }
+    std::cout << "Введите пароль (нажмите ENTER если его нет): ";
+    std::getline(std::cin, password);
 
-    // if (name[0] != '@')
-    //     name = "@" + name;
-    // for (auto &c : name)
-    //     c = tolower(c);
+    if (name[0] != '@')
+        name = "@" + name;
+    for (auto &c : name) c = tolower(c);
 
 #ifdef PULSAR_RSA_TEST
     if (!rsa_test(PULSAR_RSA_TEST)) return -1;
 #endif
 
-    // Client client(name, password, serverIP, PULSAR_PORT);
-    // client.run();
+    Client client(name, password, serverIP, PULSAR_PORT);
+    client.run();
 
     std::cout << "Клиент завершил свою работу." << std::endl;
 
